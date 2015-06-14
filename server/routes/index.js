f17afffde77e937ff521bf39987afa53a4545cf3
Mayank Gupta <mayankg1992@gmail.com>
2015-06-15 00:09:36 +0530

    PUT /todolist/:id done

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,7 +11,28 @@ router.get('/todolist', function(req, res) {
     var collection = db.get('todolist');
     collection.find({},{},function(e,docs){
         res.json({
+        	'status' : 0,
         	'todolist' : docs
+        });
+    });
+});
+
+//Create a new item in list
+router.post('/todolist', function(req, res) {
+	var db = req.db;
+	var todo = req.body.todo;
+    var collection = db.get('todolist');
+    collection.insert({
+    	"todo": todo
+    }, function(e,doc){
+    	if(e) {
+    		res.json({
+    			'status' : 1
+    		});
+    	}
+        res.json({
+        	'status' : 0,
+        	'todoitem' : doc
         });
     });
 });
@@ -21,11 +42,36 @@ router.get('/todolist/:id', function(req, res) {
 	var db = req.db;
 	var id = req.params.id;
     var collection = db.get('todolist');
-    collection.findOne({"id": id},{},function(e,docs){
+    collection.findOne({"id": id},{},function(e,doc){
         res.json({
-        	'todoitem' : docs
+        	'status' : 0,
+        	'todoitem' : doc
         });
     });
 });
+
+//Update an item in list
+router.put('/todolist/:id', function(req, res) {
+	var db = req.db;
+	var id = req.params.id;
+	var todo = req.body.todo;
+    var collection = db.get('todolist');
+    collection.updateById(id,{
+    	$set: {
+    		"todo": todo
+    	}
+    }, function(e,doc){
+    	if(e) {
+    		res.json({
+    			'status' : 1
+    		});
+    	}
+        res.json({
+        	'status' : 0,
+        	'todoitem' : doc
+        });
+    });
+});
+
 
 module.exports = router;
